116c90ed08c708da7f36c5f913a8b8212097e011
Evgeniia Dmitrieva <be230497@gmail.com>
2022-10-16 20:12:45 +0700

    Отрисовала элемент и заполнила его данными

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-import './offer.js';
+import { createOffers } from './offer.js';
+import { offerToCard } from './createSimilarElement.js';
+
+const offers = createOffers();
+const cards = offers.map(offerToCard);
+
+const blockMap = document.querySelector('#map-canvas');
+blockMap.append(cards[0]);
