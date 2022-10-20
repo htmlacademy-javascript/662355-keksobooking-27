0a21ec6c2d72b97ad048adc5ef1cc68a66c13c97
Evgeniia Dmitrieva <be230497@gmail.com>
2022-10-20 20:42:03 +0700

    Добавила валидацию формы

--- js/form.js
+++ js/form.js
@@ -1,3 +1,41 @@
+const form = document.querySelector('.ad-form');
+const pristine = new Pristine(form, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element--invalid',
+  errorTextParent: 'ad-form__element',
+  errorTextClass: 'text-help'
+});
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  pristine.validate();
+});
+
+const roomsField = form.querySelector('#room_number');
+const capacityField = form.querySelector('#capacity');
+
+const roomErrors = {
+  1: 'Только для 1 гостя',
+  2: 'Для 2 гостей и меньше',
+  3: 'Для 3 гостей и меньше',
+  100: 'Не для гостей'
+};
+
+const roomsOption = {
+  1: ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0']
+};
+
+const getRoomsErrorMessage = () => roomErrors[roomsField.value];
+
+const validateRooms = () => roomsOption[roomsField.value].includes(capacityField.value);
+
+pristine.addValidator(roomsField, validateRooms, getRoomsErrorMessage);
+pristine.addValidator(capacityField, validateRooms);
+
 const switchStateElements = (elements, state) => {
   elements.forEach((element) => {
     element.disabled = state;
@@ -5,7 +43,6 @@ const switchStateElements = (elements, state) => {
 };
 
 const switchStateForm = (state) => {
-  const form = document.querySelector('.ad-form');
   const fieldsets = form.querySelectorAll('fieldset');
   form.classList.toggle('ad-form--disabled', state);
   switchStateElements(fieldsets, state);
