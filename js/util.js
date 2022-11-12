0713696ad12c8fda0273a890c4816d3c630f7013
Evgeniia Dmitrieva <be230497@gmail.com>
2022-11-12 20:50:54 +0700

    Удалила не используемые функции

--- js/util.js
+++ js/util.js
@@ -1,31 +1,10 @@
 const ALERT_SHOW_TIME = 4000;
-
 const DEFAULT_COORDINATE = {
   lat: 35.6895000,
   lng: 139.6917100,
 };
 
 const mapCoorToText = (coor) => `${coor.lat.toFixed(5)}, ${coor.lng.toFixed(5)}`;
-
-function getRandomInt(min, max) {
-  const newMin = Math.ceil(min);
-  const newMax = Math.floor(max);
-  return getRandomFloat(newMin, newMax, 0);
-}
-
-function getRandomFloat(min, max, digits) {
-  return (min < 0 || max < 0 || max < min || isNotNumber(min) || isNotNumber(max) || isNotNumber(digits))
-    ? NaN
-    : +(Math.random() * (max - min) + min).toFixed(digits);
-}
-
-function isNotNumber(param) {
-  return typeof param !== 'number';
-}
-
-const prependZero2 = (number) => (number < 10 ? '0' : '') + number;
-
-const getRandomElement = (array) => array[getRandomInt(0, array.length - 1)];
 
 const showAlert = (message) => {
   const alertContainer = document.createElement('div');
@@ -46,4 +25,4 @@ const debounce = (callback, timeoutDelay = 500) => {
   };
 };
 
-export { getRandomInt, getRandomFloat, prependZero2, getRandomElement, showAlert, debounce, DEFAULT_COORDINATE, mapCoorToText };
+export { showAlert, debounce, DEFAULT_COORDINATE, mapCoorToText };
